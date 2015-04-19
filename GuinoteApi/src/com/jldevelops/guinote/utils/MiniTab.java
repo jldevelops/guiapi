573ec5d0a3f54df2ce51bc2d797c2097358254bb
jorge <jldevelops@gmail.com>
2015-04-19 20:35:10 +0200

    MiniTab

--- GuinoteApi/src/com/jldevelops/guinote/utils/MiniTab.java
+++ GuinoteApi/src/com/jldevelops/guinote/utils/MiniTab.java
@@ -27,10 +27,11 @@ public class MiniTab implements Serializable {
 	 */
 	final private int u;
 	final private boolean jd;
+	final private int cr;
 
 	public MiniTab(Jugador jugador, int[] puntuaciones, boolean[][] cantes,
 			int idJugActual, int haempezado, char palotriunfo,
-			Carta[] cartasMesa, int ultBaza, boolean juegoDe4) {
+			Carta[] cartasMesa, int ultBaza, boolean juegoDe4,int cr) {
 		super();
 		this.j = jugador;
 		this.p = puntuaciones;
@@ -41,19 +42,18 @@ public class MiniTab implements Serializable {
 		this.me = cartasMesa;
 		this.u = ultBaza;
 		this.jd = juegoDe4;
+		this.cr = cr;
 	}
 	
 	
 	public Jugador getJugador() {
 		return j;
 	}
-	public int[] getPuntuaciones() {
-		return p;
+	public boolean isCantado(char palo, int idjug) {
+		return c[aQueBaza(idjug)][Utils.paloInt(palo)];
 	}
-	public boolean[][] getCantes() {
-		return c;
-	}
-	public int getIdJugActual() {
+
+	public int getTurno() {
 		return i;
 	}
 	public int getHaempezado() {
@@ -62,9 +62,6 @@ public class MiniTab implements Serializable {
 	public char getPalotriunfo() {
 		return pt;
 	}
-	public Carta[] getCartasMesa() {
-		return me;
-	}
 	public int getUltBaza() {
 		return u;
 	}
@@ -72,6 +69,72 @@ public class MiniTab implements Serializable {
 		return jd;
 	}
 	
+	public int getCartasRestantes(){
+		return cr;
+	}
 	
+	private int aQueBaza(int idjug) {
+		if (jd) {
+			if (idjug == 0 || idjug == 1) {
+				return idjug;
+			} else {
+				if (idjug == 2) {
+					return 0;
+				} else {
+					return 1;
+				}
+			}
+		} else {
+			return idjug;
+		}
+	}
+	
+	public int getPuntuacion(int idjug) {
+		return p[aQueBaza(idjug)];
+	}
+	
+	/** devuelve el id del jugador siguiente (el de la derecha) a idjug
+	 * @param idjug ID del jugador
+	 * @return int
+	 */
+	public int s(int idjug) {
 
+
+		if (jd) {
+			if (idjug == 3) {
+				return 0;
+			} else {
+				 idjug++;
+				return idjug;
+			}
+		} else {
+			if (idjug == 0) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	}
+
+	/** Devuelve la carta de la mesa del jugador idjug
+	 * @param idjug
+	 * @return Carta
+	 */
+	public Carta getCartaMesa(int idjug){
+		int ultBaza1 = u;
+		if(u == -1)
+			ultBaza1 = h;
+		if (ultBaza1 == idjug)
+			return me[0];
+		else {
+			if (s(ultBaza1) == idjug)
+				return me[1];
+			else {
+				if (s(s(ultBaza1)) == idjug)
+					return me[2];
+				else
+					return me[3];
+			}
+		}
+	}
 }
